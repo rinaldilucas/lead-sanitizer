f3691afe653d6d4aee015aeffe5a174aaf9d4283
Lucas Rinaldi <lucasreinaldi@gmail.com>
2022-10-27 02:42:23 -0300

    adds new feature to remove duplicated lines

--- src/app/components/uploaders/uploaders.component.ts
+++ src/app/components/uploaders/uploaders.component.ts
@@ -8,8 +8,12 @@ import { Component } from '@angular/core';
 export class UploadersComponent {
     loadedDatabase: string[] = [];
     loadedLeads: string[] = [];
+    loadedProfiles: string[] = [];
     preparedLeads: string[] = [];
     preparedDatabase: string[] = [];
+    profileFileTitle: string;
+    databaseFileTitle: string;
+    leadsFileTitle: string;
     listSize = 300;
     isPrepared = false;
 
@@ -20,7 +24,6 @@ export class UploadersComponent {
         const self = this;
 
         fileReader.onloadend = function (fileLoadedEvent) {
-            debugger;
             const array = (fileLoadedEvent.target?.result as string).split('\n');
 
             array.forEach((element) => {
@@ -35,6 +38,7 @@ export class UploadersComponent {
         };
 
         fileReader.readAsText(fileToLoad, 'UTF-8');
+        this.databaseFileTitle = $event.files[0].name;
     }
 
     uploadLeads ($event) {
@@ -60,6 +64,33 @@ export class UploadersComponent {
         };
 
         fileReader.readAsText(fileToLoad, 'UTF-8');
+        this.leadsFileTitle = $event.files[0].name;
+    }
+
+    uploadFile ($event) {
+        const fileToLoad = $event.files[0];
+        const fileReader = new FileReader();
+        const sanitizedLines: string[] = [];
+        const self = this;
+
+        fileReader.onloadend = function (fileLoadedEvent) {
+            const array = (fileLoadedEvent.target?.result as string).split('\n');
+
+            array.forEach((element) => {
+                if (element !== '\r' && element !== ',\r' && element !== '') {
+                    const newElement = element.replace(/\r?\n|\r/g, '');
+                    sanitizedLines.push(newElement);
+                }
+            });
+
+            const filteredList = sanitizedLines.filter((element, index) => sanitizedLines.indexOf(element) === index);
+            const list: string[] = [];
+            filteredList.forEach(element => list.push(element + '\r'));
+            self.loadedProfiles = list;
+        };
+
+        fileReader.readAsText(fileToLoad, 'UTF-8');
+        this.profileFileTitle = $event.files[0].name;
     }
 
     prepareLeads () {
@@ -77,15 +108,19 @@ export class UploadersComponent {
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.setAttribute('href', url);
-        link.setAttribute('download', filename + '.txt');
+        link.setAttribute('download', filename);
         link.style.visibility = 'hidden';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     }
 
-    downloadDatabase () {
-        this.downloadFile(this.preparedDatabase, 'database');
+    downloadDatabase (title: string) {
+        this.downloadFile(this.preparedDatabase, title);
+    }
+
+    downloadProfiles (title: string) {
+        this.downloadFile(this.loadedProfiles, title);
     }
 
     downloadLeads () {
@@ -93,7 +128,7 @@ export class UploadersComponent {
 
         chunkedList.forEach((list: any, index) => {
             const date = new Date().toISOString().slice(0, 10);
-            this.downloadFile(list, date + '_leads-' + ++index);
+            this.downloadFile(list, date + '_leads-' + ++index + '.txt');
         });
     }
 
